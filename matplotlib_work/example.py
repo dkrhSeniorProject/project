5192378c3912f7ea3e9a94144fd643d9bb1e8795
rhong01 <ronald.hong@tufts.edu>
2013-04-10 00:19:40 -0400

    rc1!

--- matplotlib_work/example.py
+++ matplotlib_work/example.py
@@ -29,3 +29,24 @@ class Map:
 		print("Names of ScatterSets in scatter:")
 		for entry in self.scatter:
 			print(entry)
+			
+y = Map(xmin=0, xmax=9, ymin=0, ymax=9, windowWidth=6, windowHeight=6) # create a map object
+y.newScatter('red', 'ro') # create a scatterplot set called 'red' with red circles as markers ('ro')
+y.scatter['red'].update_set([1],[1]) # set xy coordinates (1,1) for entries in 'red'
+y.newScatter('blue', 'bo')
+y.scatter['blue'].update_set([2],[2])
+
+y.printScatter() # prints the names of the scatterplot sets
+y.fig.canvas.draw() # each time this is called, the plot is redrawn
+print("Press enter to continue.")
+raw_input()
+####
+y.scatter['red'].update_set([1, 2, 3], [1, 4, 9])
+y.scatter['blue'].update_set([1.5, 2.5, 3.5], [3, 5, 7])
+y.newScatter('green', 'go')
+y.scatter['green'].update_set([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5])
+
+y.printScatter()
+y.fig.canvas.draw()
+print("Press enter to exit.")
+raw_input()
